89ca50e28326c4c91e252d64147a352bcc01e1b7
chaplinho20-cmd <chaplinho20@gmail.com>
2025-12-12 20:07:28 -0300

    docs: anotação

--- 01 anotacao.py
+++ 01 anotacao.py
@@ -1,6 +1,7 @@
-#"""sequencia de envio para o git
+#sequencia de envio para o git
 #1. git status
 #2. git add .
 #3. git status
 #4. git commit -m
-#5. git push origin master"""
+#5. git push origin master
+#6. git pull origin master
